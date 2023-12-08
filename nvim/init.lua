f86e59882f120f9b98e243765e43eadf11ae0073
bazileros <zalisilesithule@gmail.com>
2023-12-08 15:21:39 +0200

    added neoSolarized as default theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
 vim.opt.termguicolors = true
---vim.g.nightflyTransparent = true
+vim.g.nightflyTransparent = true
 
 -- Colorschemess Section
 
--- vim.cmd("colorscheme onedark")
--- vim.cmd("colorscheme NeoSolarized")
+--vim.cmd("colorscheme onedark")
+vim.cmd("colorscheme NeoSolarized")
 --vim.cmd("colorscheme rose-pine")
--- vim.cmd("colorscheme catppuccin-mocha")
+--vim.cmd("colorscheme catppuccin-mocha")
 -- vim.cmd("colorscheme melange")
-vim.cmd("colorscheme nightfly")
+--vim.cmd("colorscheme nightfly")
 --vim.cmd("colorscheme kanagawa")
 --vim.cmd("colorscheme gruvbox")
 -- vim.cmd("colorscheme nyoom")
+--vim.cmd("colorscheme monokai-pro")
